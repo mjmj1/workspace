369e8cf446198be74de5da3d6bfce6b5bc8adac6
manjong <mjong3365@gmail.com>
2021-11-06 14:04:54 +0900

    master

--- slide.js
+++ slide.js
@@ -10,29 +10,55 @@ btnNext.addEventListener("click", function () {
     const slideWidth = 400;
 
     if (curIndex <= itemLen - 1) {
-        moveSlide(curIndex);
+        moveSlideNext(curIndex);
+        console.log(itemLen);
     }
-    if (curIndex === count - 1) {
+    if (curIndex === itemLen - 3) {
         setTimeout(function () {
+            const list = document.querySelector(".list");
+
             list.style.left = -slideWidth + "px";
             list.style.transition = `${0}s ease-out`;
         }, 500);
         curIndex = -1;
     }
     curIndex += 1;
+    console.log(curIndex);
 });
 btnPrev.addEventListener("click", function () {
+    const item = document.querySelectorAll(".item");
+    const itemLen = item.length;
+    const slideWidth = 400;
+
     if (curIndex >= 0) {
-        list.style.left = -curIndex;
+        moveSlidePrev(curIndex);
+        console.log(itemLen);
     }
-    moveSlide(curIndex - 1);
+    if (curIndex === 0) {
+        setTimeout(function () {
+            const list = document.querySelector(".list");
+
+            list.style.left = -(itemLen - 2) * slideWidth + "px";
+            list.style.transition = `${0}s ease-out`;
+        }, 500);
+        curIndex = itemLen - 2;
+    }
+    curIndex -= 1;
+    console.log(curIndex);
 });
 
-function moveSlide(num) {
+function moveSlideNext(num) {
     const list = document.querySelector(".list");
     const slideWidth = 400;
 
     list.style.left = -(num + 2) * slideWidth + "px";
-    list.style.transition = `${0.5}s ease-out`;
-    curIndex = num;
+    list.style.transition = `${0.3}s ease-out`;
+}
+
+function moveSlidePrev(num) {
+    const list = document.querySelector(".list");
+    const slideWidth = 400;
+
+    list.style.left = -num * slideWidth + "px";
+    list.style.transition = `${0.3}s ease-out`;
 }
